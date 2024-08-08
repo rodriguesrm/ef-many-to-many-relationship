490ef8682152b53b25dc18e418c303d1b9125d20
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2024-08-08 18:31:05 -0300

    Adicionado endpoint de GET /api/pessoa/{id}

--- PockEfCoreApi/Controllers/PessoaController.cs
+++ PockEfCoreApi/Controllers/PessoaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
 using PockEfCoreApi.Infra.Data;
 using PockEfCoreApi.Infra.Data.Tables;
 using PockEfCoreApi.Models;
@@ -27,7 +28,7 @@ namespace PockEfCoreApi.Controllers
                 Nascimento = request.Nascimento
             };
 
-            Tipo tipo = _ctx.Tipos.FirstOrDefault(tipo => tipo.Nome == request.Tipo);
+            Tipo? tipo = _ctx.Tipos.FirstOrDefault(tipo => tipo.Nome == request.Tipo);
             if (tipo == null)
                 tipo = new Tipo() { Nome = request.Tipo };
             pessoa.Tipos.Add(tipo);
@@ -38,6 +39,25 @@ namespace PockEfCoreApi.Controllers
             IActionResult result = Created(string.Empty, new { Id = pessoa.Id });
 
             return Task.FromResult(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public Task<IActionResult> ConsultarPessoa([FromRoute] int id)
+        {
+
+            var pessoa = _ctx.Pessoas.Find(id);
+            IActionResult result = 
+                (pessoa != null)
+                ? Ok(new {
+                    Id = pessoa.Id,
+                    Nome = pessoa.Nome,
+                    Nascimento = pessoa.Nascimento.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)
+                })
+                : NotFound()
+            ;
+
+            return Task.FromResult(result);
+
         }
 
     }
